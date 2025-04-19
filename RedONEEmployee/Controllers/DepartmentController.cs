d538e4c0dd58bcee276c6cdd492d19af3ed779cf
JeffCSZ <siawzheng1999@gmail.com>
2025-04-19 16:38:26 +1000

    There are two addDepartment Controller, there are two way of getting departmentID FromRoute, FromQuery

---
+++ RedONEEmployee/Controllers/DepartmentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RedONEEmployee.Data;
+
+namespace RedONEEmployee.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController(MyDBContext _context) : ControllerBase
+    {
+        [HttpGet("GetAllDepartments")]
+        public ActionResult GetAllDepartments()
+        {
+            var depts = _context.Departments.ToList();
+            return Ok(depts);
+        }
+
+        // From Route
+        [HttpGet("GetDepartment/{id:int}")]
+        public ActionResult GetDepartment(int id)
+        {
+            var dept = _context.Departments.FirstOrDefault(d => d.Id == id);
+            return Ok(dept);
+        }
+
+        // From Query
+        [HttpGet("GetDepartment2")]
+        public ActionResult GetDepartment2(int id)
+        {
+            var dept = _context.Departments.FirstOrDefault(d => d.Id == id);
+            return Ok(dept);
+        }
+    }
+}
